ef29b286c12a63fd08980a846be1e35e6c43c5a3
el-zach <el-zach@web.de>
2019-06-01 17:11:52 +0200

    Added Bot Data Structure

---
+++ Assets/Scripts/BotConfiguator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BotConfiguator : MonoBehaviour
+{
+    public BotData defaultBot;
+
+    private void Start()
+    {
+        GenerateBotFromData(defaultBot);
+    }
+
+    public void GenerateBotFromData(BotData bot)
+    {
+        var wheels = InstantiateFromPart(bot.wheels);
+        var chassis = InstantiateFromPart(bot.chassis, wheels.transform);
+        InstantiateFromPart(bot.attachements[0], chassis.transform);
+    }
+
+    public GameObject InstantiateFromPart(PartData part, Transform attachedTo = null)
+    {
+        Transform parent = attachedTo ? attachedTo : transform;
+        GameObject clone = Instantiate(part.prefab, parent);
+
+        return clone;
+    }
+
+}
---
+++ Assets/Scripts/Bots/LogicData.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class LogicData : ScriptableObject
+{
+    [System.Serializable]
+    public class LogicBlock
+    {
+
+        public Vector3 rotation;
+
+        public void Execute(PartData part)
+        {
+
+        }
+
+    }
+
+    public List<LogicBlock> logicBlocks;
+
+    public void Execute(PartData part)
+    {
+        foreach(var block in logicBlocks)
+        {
+            block.Execute(part);
+        }
+    }
+
+}
