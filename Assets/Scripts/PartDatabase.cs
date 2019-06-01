65fcd87fbd2857e2024146923adb789a7b9d50ff
el-zach <el-zach@web.de>
2019-06-01 17:16:42 +0200

    Added PartDatabase script

---
+++ Assets/Scripts/PartDatabase.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PartDatabase : MonoBehaviour
+{
+
+    public static PartDatabase singleton;
+    private void Awake()
+    {
+        if (singleton == null)
+            singleton = this;
+        else
+        {
+            Debug.LogWarning("PartDatabase already exists!", this);
+        }
+    }
+
+    public List<PartData> parts;
+}
