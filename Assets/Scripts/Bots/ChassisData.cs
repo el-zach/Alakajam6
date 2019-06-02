7e4361522c4651f98f63592a9652489ed421ea98
el-zach <el-zach@web.de>
2019-06-02 15:03:15 +0200

    Added shooting scripts

--- Assets/Scripts/Bots/ChassisData.cs
+++ Assets/Scripts/Bots/ChassisData.cs
@@ -5,5 +5,7 @@ using UnityEngine;
 [CreateAssetMenu]
 public class ChassisData : PartData {
     public float rotationalSpeed = 1f;
-
+    public float fireRateBonus = 1f;
+    public float salveCountBonus = 1f;
+    public float salveCooldownBonus = 1f;
 }
--- Assets/Scripts/Bots/WeaponData.cs
+++ Assets/Scripts/Bots/WeaponData.cs
@@ -1,4 +1,10 @@
 ﻿using UnityEngine;
 
 [CreateAssetMenu]
-public class WeaponData : PartData { }
+public class WeaponData : PartData {
+    //public float damage = 1f;
+    public float fireRate = 3f;
+    public int salveCount = 5;
+    public float salveCooldown = 1f;
+
+}
