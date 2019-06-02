799d45de6e8bf6c910eb627df0cb2c253f60d991
el-zach <el-zach@web.de>
2019-06-02 11:49:21 +0200

    Added font and disabled upload til bot is named

--- Assets/Scripts/Bot.cs
+++ Assets/Scripts/Bot.cs
@@ -58,6 +58,9 @@ public class Bot : MonoBehaviour
         currentSpurt = spurtDuration;
         spurtCooldown = data.wheels.spurtCooldown;
 
+        //----chassis---//
+        rotationalSpeed = data.chassis.rotationalSpeed;
+
     }
 
     private void Update()
